42934d41711fa9574ab89713d66d06709d65d1b3
ro14hit <rohit1499singh@gmail.com>
2024-03-14 18:39:32 +0530

    Added camera positioning and animation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,6 +23,14 @@ const Home = () => {
               alt="logo"
               className="w-8 h-8 object-contain"
             />
+            <CustomButton
+                type="filled"
+                title="GitHub"
+                handleClick={() =>
+                  window.open("https://github.com/ro14hit/ClothAI", "_blank")
+                }
+                customStyles="w-fit px-4 mt-3 py-2.5 font-bold text-sm rounded-3xl"
+              />
           </motion.header>
 
           <motion.div className="home-content" {...headContainerAnimation}>
@@ -37,8 +45,9 @@ const Home = () => {
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
                 Create your unique and exclusive shirt with my brand-new 3D
-                customization tool. <strong>Unleash your imagination</strong>{" "}
-                and define your own style.
+                customization tool <b className="text-yellow-800">CLOTHAI</b>.{" "}
+                <strong>Unleash your imagination</strong> and define your own
+                style.
               </p>
 
               <CustomButton
